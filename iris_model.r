b7006bd6b0138e784883d8a9bb3df0aaca0d88da
Doniele Arys Antonio <arysantonio363@gmail.com>
2025-03-27 14:51:19 +0800

    added training datasets

--- iris_model.r
+++ iris_model.r
@@ -7,3 +7,12 @@ library(dplyr)           # For data manipulation
 
 set.seed(123)
 
+data(iris)
+cat("First few rows of the dataset:\n")
+print(head(iris))
+cat("\nSummary of the dataset:\n")
+print(summary(iris))
+
+train_index <- createDataPartition(iris$Species, p = 0.7, list = FALSE)
+train_data <- iris[train_index, ]
+test_data <- iris[-train_index, ]
