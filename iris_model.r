185970d95f2450b461710d79f3fc6bce42f08362
Doniele Arys Antonio <arysantonio363@gmail.com>
2025-03-27 14:46:30 +0800

    hello cc19

---
+++ iris_model.r
@@ -0,0 +1,6 @@
+.libPaths("C:/Users/Donielr Arys Antonio/R-CC19")
+
+library(randomForest)    # For random forest model
+library(ggplot2)         # For visualization
+library(caret)           # For model training and evaluation
+library(dplyr)           # For data manipulation
